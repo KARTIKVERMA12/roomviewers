8e4aa711a7653dd9eacefc0a430581f877c4ae92
Kartik Verma <130089298+KARTIKVERMA12@users.noreply.github.com>
2024-03-06 00:35:19 +0530

    inital commit

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className="bg-red-800 pb-16">
+      <div className="container mx-auto flex flex-col ">
+        {/* <h1 className="text-5xl text-white font-bold">Find your next stay</h1> */}
+        <h1 className="text-3xl text-white font-bold">          Search low prices on hotels and Rooms...
+</h1>
+
+        {/* <p className="text-2xl text-white">
+          Search low prices on hotels and Rooms...
+        </p> */}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
